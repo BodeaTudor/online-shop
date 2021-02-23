88005f5acf44e4bedd57d6f1a991d4fd7d5be471
Bodea Tudor <bodea.tudor99@yahoo.com>
2021-02-23 19:12:49 +0200

    *added a controller for the customer class with endpoints for POST, GET and PUT methods

---
+++ src/main/java/com/example/onlineshop/web/CustomerController.java
@@ -0,0 +1,47 @@
+package com.example.onlineshop.web;
+
+import com.example.onlineshop.domain.Customer;
+import com.example.onlineshop.service.CustomerService;
+import com.example.onlineshop.transfer.customer.SaveCustomerRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer(@RequestBody @Valid SaveCustomerRequest request) {
+
+        Customer createdCustomer = customerService.createCustomer(request);
+
+        return new ResponseEntity<>(createdCustomer, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable("id") long id) {
+
+        Customer customer = customerService.getCustomer(id);
+
+        return new ResponseEntity<>(customer, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable("id") long id, @RequestBody @Valid SaveCustomerRequest request) {
+
+        Customer updatedCustomer = customerService.updateCustomer(id, request);
+
+        return new ResponseEntity<>(updatedCustomer, HttpStatus.OK);
+    }
+}
