de59961b54b08b27661febae949848b4cb8a5eb2
Bodea Tudor <bodea.tudor99@yahoo.com>
2021-02-23 12:21:49 +0200

    *added transfer layer with a DTO for creating and updating products

---
+++ src/main/java/com/example/onlineshop/transfer/SaveProductRequest.java
@@ -0,0 +1,25 @@
+package com.example.onlineshop.transfer;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+public class SaveProductRequest {
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private int quantity;
+
+    @NotNull
+    private double price;
+
+    @NotNull
+    private String description;
+
+    @NotNull
+    private String imagePath;
+
+}
