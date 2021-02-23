50ce9d12bda375abda6922ec27155d4da64524e7
Bodea Tudor <bodea.tudor99@yahoo.com>
2021-02-23 14:28:41 +0200

    *added getProducts endpoint for retrieving all products

--- src/main/java/com/example/onlineshop/web/ProductController.java
+++ src/main/java/com/example/onlineshop/web/ProductController.java
@@ -2,8 +2,11 @@ package com.example.onlineshop.web;
 
 import com.example.onlineshop.domain.Product;
 import com.example.onlineshop.service.ProductService;
+import com.example.onlineshop.transfer.GetProductsRequest;
 import com.example.onlineshop.transfer.SaveProductRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -35,5 +38,13 @@ public class ProductController {
         Product product = productService.getProduct(id);
 
         return new ResponseEntity<>(product, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<Product>> getProducts(GetProductsRequest request, Pageable pageable) {
+
+        Page<Product> products = productService.getProducts(request, pageable);
+
+        return new ResponseEntity<>(products, HttpStatus.OK);
     }
 }
